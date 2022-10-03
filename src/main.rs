597167cfeed2f836f5613966fe12ef4db63f787a
hitchhicker <yubokai8@gmail.com>
2022-10-04 00:52:01 +0200

    Intial commit, with regex definition done

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub mod prep;
+pub mod constants;
+pub mod utils;
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/prep/mod.rs
@@ -0,0 +1,6 @@
+pub mod regexes;
+pub mod text_parser;
+pub mod text_prep;
+pub mod token;
+pub mod tokenizer;
+pub mod word_segmentation;
