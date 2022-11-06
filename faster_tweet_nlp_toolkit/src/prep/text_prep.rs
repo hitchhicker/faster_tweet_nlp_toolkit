4730c58ad47aafc8ff64be3af078fa5fc3b8c99c
hitchhicker <yubokai8@gmail.com>
2022-11-06 16:08:09 +0800

    Implement minimal prep function in text_prep

---
+++ faster_tweet_nlp_toolkit/src/prep/text_prep.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+use super::{text_parser::parse_text, token::Token};
+
+pub fn prep(
+    text: String,
+    encoding: Option<&str>,
+    remove_unencodable_char: Option<bool>,
+    to_lower: Option<bool>,
+    strip_accents: Option<bool>,
+    reduce_len: Option<bool>,
+    tokenizer: Option<fn(String) -> Vec<Token>>,
+    filters: Option<HashSet<&str>>,
+    emojis: Option<&str>,
+    mentions: Option<&str>,
+    hashtags: Option<&str>,
+    urls: Option<&str>,
+    digits: Option<&str>,
+    puncts: Option<&str>,
+    emails: Option<&str>,
+    html_tags: Option<&str>,
+) -> String {
+    return parse_text(
+        text,
+        encoding,
+        remove_unencodable_char,
+        to_lower,
+        strip_accents,
+        reduce_len,
+        tokenizer,
+        filters,
+        emojis,
+        mentions,
+        hashtags,
+        urls,
+        digits,
+        puncts,
+        emails,
+        html_tags,
+    ).value.unwrap()
+}
